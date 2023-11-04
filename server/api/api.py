3df07f22bbb254f39f666025218b5ac273d08f6c
rishixyz9 <reach2rishabh@rocketmail.com>
2023-11-04 18:21:34 -0500

    schema and ideas

--- server/api/api.py
+++ server/api/api.py
@@ -1,3 +1,9 @@
 # 1. send user's response to gpt
 # 2. get gpt's reponse and make recommendations
-# 3. design recommendation system
+# 3. design recommendation system 
+
+
+
+# gpt breaks down input to scores
+# ex:
+# [(commercial auto, .8), (renters, .1), etc..]
